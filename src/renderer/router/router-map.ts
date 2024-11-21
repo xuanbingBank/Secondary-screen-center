efd0348b24286af3e580def0e2ccd99624a9db49
玄冰 <1300272284@qq.com>
2024-11-21 21:41:32 +0800

    给当前页面添加图标

--- src/renderer/router/router-map.ts
+++ src/renderer/router/router-map.ts
@@ -3,7 +3,14 @@ import {
   HomeOutlined,
   AppstoreOutlined,
   SettingOutlined,
+  ExperimentOutlined,
+  DownloadOutlined,
+  GlobalOutlined,
+  FileSearchOutlined,
+  BugOutlined,
+  AppstoreAddOutlined
 } from '@ant-design/icons-vue';
+import { tourStepInfo } from "ant-design-vue/es/vc-tour";
 
 const routeMap: Array<RouteRecordRaw> = [
   {
@@ -25,13 +32,19 @@ const routeMap: Array<RouteRecordRaw> = [
         path: "download",
         name: "download",
         component: () => import("../views/feature/download.vue"),
-        meta: { title: "文件下载" }
+        meta: { 
+          title: "文件下载",
+          icon: DownloadOutlined
+        }
       },
       {
         path: "request",
         name: "request",
         component: () => import("../views/feature/request.vue"),
-        meta: { title: "网络请求" }
+        meta: { 
+          title: "网络请求",
+          icon: GlobalOutlined
+        }
       }
     ]
   },
@@ -48,16 +61,44 @@ const routeMap: Array<RouteRecordRaw> = [
     redirect: "/tools/md5",
     children: [
       {
-        path: "md5",
+        path: "/md5",
         name: "md5",
         component: () => import("../views/tools/md5.vue"),
-        meta: { title: "文件MD5" }
+        meta: { 
+          title: "文件MD5",
+          icon: FileSearchOutlined
+        }
       },
       {
-        path: "debug",
+        path: "/debug",
         name: "debug",
         component: () => import("../views/tools/debug.vue"),
-        meta: { title: "调试工具" }
+        meta: { 
+          title: "调试工具",
+          icon: BugOutlined
+        }
+      }
+    ]
+  },
+  {
+    path: "/example",
+    name: "example",
+    component: () => import("../views/primary.vue"),
+    meta: {
+      title: "示例",
+      icon: ExperimentOutlined,
+      primary: true
+    },
+    redirect: "/example/components-overview",
+    children: [
+      {
+        path: "components-overview",
+        name: "components-overview",
+        meta: { 
+          title: "组件总览",
+          icon: AppstoreAddOutlined
+        },
+        component: () => import("../views/example/ant-design-vue/components-overview.vue"),
       }
     ]
   }
